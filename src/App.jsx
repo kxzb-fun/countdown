cd7ae8910e1d2c4d269ac515340efaac5d47ea1b
kxzb-fun <sunday_ever@163.com>
2024-07-18 23:56:51 +0800

    feat: add dependencies and config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import EventList from './pages/EventList';
+import EventDetails from './pages/EventDetails';
+import EventForm from './pages/EventForm';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h2>Important Time</h2>
+      <Header />
+      <Routes>
+        <Route path="/" element={<EventList />} />
+        <Route path="/event/:id" element={<EventDetails />} />
+        <Route path="/new" element={<EventForm />} />
+        <Route path="/edit/:id" element={<EventForm />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
